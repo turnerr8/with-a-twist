b08f32751eb88d348fdac40e2098d97cd1287293
Ryan Turner <turnerr8@wwu.edu>
2023-11-06 16:26:26 -0800

    adding more nav styles

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import Nav from '../Components/Nav';
 import { getImages } from '../firebaseConfig';
 import { useEffect, useState } from 'react';
@@ -6,28 +8,77 @@ import { Loader } from '../Components/Loader';
 export default function Gallery() {
 	const [imageArray, setImageArray] = useState([]);
 	const [isLoaded, setIsLoaded] = useState(false);
+
 	useEffect(() => {
 		getImages().then((imgs) => {
 			setImageArray(imgs);
 			setIsLoaded(true);
 		});
 	}, []);
+
 	return (
 		<>
 			{!isLoaded && <Loader />}
 			<Nav />
 			<div className=' bg-[#181C11] grid md:grid-cols-gallery sm:grid-cols-gallery-md gap-[20px] px-12 py-12'>
 				{imageArray.map((img) => {
-					return (
-						<div
-							key={img.id}
-							className=' w-full h-full object-cover transition-transform ease-in-out hover:scale-105 '
-						>
-							<img src={img.imageURL} alt='image of trailer'></img>
-						</div>
-					);
+					return <Image img={img} key={img.id} />;
 				})}
 			</div>
 		</>
+	);
+}
+
+function Image({ img }) {
+	const [isExpanded, setIsExpanded] = useState();
+
+	const toggleModal = () => {
+		if (isExpanded) {
+			setIsExpanded(false);
+			document.body.style.overflow = 'unset';
+		} else {
+			setIsExpanded(true);
+			if (typeof window !== 'undefined' && window.document) {
+				document.body.style.overflow = 'hidden';
+			}
+		}
+	};
+
+	return (
+		<>
+			{isExpanded && <Modal url={img.imageURL} toggleModal={toggleModal} />}
+			<div
+				key={img.id}
+				className=' w-full h-full object-cover transition-transform ease-in-out hover:scale-105 cursor-pointer'
+				onClick={toggleModal}
+			>
+				<img src={img.imageURL} alt='image of trailer'></img>
+			</div>
+		</>
+	);
+}
+
+function Modal({ url, toggleModal }) {
+	return (
+		<div className='w-screen h-screen bg-[#000000bc] z-50  fixed top-0 left-0 overflow-hidden flex items-center justify-center py-16 px-8 sm:px-24  '>
+			<svg
+				className=' fill-white h-12 absolute right-12 top-8 cursor-pointer hover:opacity-60'
+				onClick={toggleModal}
+				clipRule='evenodd'
+				fillRule='evenodd'
+				strokeLinejoin='round'
+				strokeMiterlimit='2'
+				viewBox='0 0 24 24'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<path d='m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z' />
+			</svg>
+
+			<img
+				src={url}
+				alt='with a twist image'
+				className=' w-full h-full object-contain '
+			/>
+		</div>
 	);
 }
